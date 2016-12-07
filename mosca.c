8ba18b17aec3713bb79dff49f52af1660f0831e1
Cooler_ <coolerlair@gmail.com>
2016-12-07 05:53:21 -0200

    Update mosca.c

--- mosca.c
+++ mosca.c
@@ -25,7 +25,7 @@ void init_banner_mosca()
 "       .'(_I_)`.   |  | |__| ___] |___ |  |   coded by Cooler_\n"
 "           \"       \n"
  YELLOW
-"    Static analysis tool to find bugs like a grep unix command  v0.05 \n"
+"    Static analysis tool to find bugs like a grep unix command  v0.06 \n"
  LAST
 "+-----------------------------------------------------------------+\n"
 "Options: \n"
